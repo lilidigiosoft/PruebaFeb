e2e3ea23a527d09d84c79151eb57ac84f5fee0ee
digiopractica <lilia.rabago@digiosoft.com.mx>
2024-02-07 12:54:02 -0700

    variable

--- mvc/index.php
+++ mvc/index.php
@@ -2,8 +2,10 @@
 require_once("config.php");
 require_once("controlador/index.php");
 
-if(isset($_GET['m'])):
+if(isset($_GET['m']))://BUSCA LA VARIABLE
+  
      if(method_exists(modeloController,$_GET['m']))
+
         modeloController::{$_GET['m']}();
 else
      modeloController::index();
